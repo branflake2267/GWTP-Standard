fde2b57d88d4c0e645e0d681ec01441dc1bb9588
Brandon Donnelson <branflake2267@gmail.com>
2013-07-14 10:06:49 -0700

    Standard project upload

---
+++ GWTP-Standard/src/com/arcbees/project/client/place/NameTokens.java
@@ -0,0 +1,18 @@
+package com.arcbees.project.client.place;
+
+/**
+ * The central location of all name tokens for the application. All {@link ProxyPlace} classes get their tokens from
+ * here. This class also makes it easy to use name tokens as a resource within UIBinder xml files.
+ * <p />
+ * The public static final String is used within the annotation {@link NameToken}, which can't use a method and the
+ * method associated with this field is used within UiBinder which can't access static fields.
+ * <p />
+ * Also note the exclamation mark in front of the tokens, this is used for search engine crawling support.
+ */
+public class NameTokens {
+    public static final String home = "!home";
+
+    public static String getHome() {
+        return home;
+    }
+}
